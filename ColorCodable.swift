e8a2a4826c1adb74ab654a62c3b3c2c9283187eb
brendon cecilio <brendon.cecilio@gmail.com>
2019-11-26 15:07:17 -0500

    UI complete/ color data completed

---
+++ ColorCodable.swift
@@ -0,0 +1,9 @@
+//
+//  ColorCodable.swift
+//  Parsing-JSON-Lab
+//
+//  Created by Brendon Cecilio on 11/26/19.
+//  Copyright © 2019 Brendon Cecilio. All rights reserved.
+//
+
+import Foundation
--- VC's/ColorViewController.swift
+++ VC's/ColorViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ColorViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
