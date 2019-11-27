41c480150173884559cede94ea528f2d078ed756
brendon cecilio <brendon.cecilio@gmail.com>
2019-11-27 11:30:56 -0500

    color VC/detailVC complete

---
+++ VC's/WeatherDetailController.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherDetailController.swift
+//  Parsing-JSON-Lab
+//
+//  Created by Brendon Cecilio on 11/27/19.
+//  Copyright © 2019 Brendon Cecilio. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WeatherCodable.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherCodable.swift
+//  Parsing-JSON-Lab
+//
+//  Created by Brendon Cecilio on 11/26/19.
+//  Copyright © 2019 Brendon Cecilio. All rights reserved.
+//
+
+import Foundation
