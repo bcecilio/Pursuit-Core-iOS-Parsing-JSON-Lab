e44bf415e868fef750913ec87dc3261e6821e2b0
brendon cecilio <brendon.cecilio@gmail.com>
2019-11-27 13:54:03 -0500

    random user generator vc / detailvc complete

---
+++ RandomUserCodable.swift
@@ -0,0 +1,9 @@
+//
+//  RandomUserCodable.swift
+//  Parsing-JSON-Lab
+//
+//  Created by Brendon Cecilio on 11/27/19.
+//  Copyright © 2019 Brendon Cecilio. All rights reserved.
+//
+
+import Foundation
